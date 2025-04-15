d10847bbdabff1adaaa4f9bb5ace0d284ab6441d
Tanushree2004 <114222406+Tanushree2004@users.noreply.github.com>
2025-04-15 20:58:08 +0530

    Update apps.py

--- classroom/apps.py
+++ classroom/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
 class ClassroomConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'classroom'
+
+    def ready(self):
+        import classroom.signals  # Ensures signal handlers are loaded
