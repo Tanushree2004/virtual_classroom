ebae7201a24caee65f8d86d0614f43e60cdaabcb
Tanushree2004 <114222406+Tanushree2004@users.noreply.github.com>
2025-04-15 21:09:17 +0530

    Update forms.py

--- resource_library/forms.py
+++ resource_library/forms.py
@@ -1,10 +1,28 @@
 from django import forms
 from .models import Resource, Category
+from classroom.models import Classroom
+from django.db import models
 
 class ResourceForm(forms.ModelForm):
     class Meta:
         model = Resource
-        fields = ['title', 'category', 'file']
+        fields = ['title', 'file', 'category']
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super(ResourceForm, self).__init__(*args, **kwargs)
+
+        if user:
+            # Get classroom titles taught by this instructor
+            classroom_titles = Classroom.objects.filter(instructor=user).values_list('title', flat=True)
+
+            if user.role == "Instructor":
+                self.fields['category'].queryset = Category.objects.filter(
+                    models.Q(name__in=classroom_titles) | models.Q(owner=user)
+                )
+
+            elif user.role == "Student":
+                self.fields['category'].queryset = Category.objects.filter(owner=user)
 
 class CategoryForm(forms.ModelForm):
     class Meta:
