5fe427f6af64547153674aeaef0ee8b4d096abd1
Tanushree2004 <114222406+Tanushree2004@users.noreply.github.com>
2025-04-15 21:21:43 +0530

    Update urls.py

--- helpbot/urls.py
+++ helpbot/urls.py
@@ -1,11 +1,11 @@
 # helpbot/urls.py
-
 from django.urls import path
 from . import views
 
+app_name = "helpbot"  # This registers the namespace
 
 urlpatterns = [
     path('', views.helpbot_home, name='helpbot_home'),
     path('ask/', views.get_help_response, name='helpbot_ask'),
-
+    path('update-name/', views.update_helpbot_name, name='update_helpbot_name'),
 ]
