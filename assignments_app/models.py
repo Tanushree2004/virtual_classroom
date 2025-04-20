dfcd3cf5fc188f4f5c095cc584153b00df27fccf
Tanushree2004 <114222406+Tanushree2004@users.noreply.github.com>
2025-04-20 20:16:11 +0530

    Add files via upload
    New assignment app

---
+++ assignments_app/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from classroom.models import Classroom
+
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    instructor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="assignments")
+    group = models.CharField(max_length=255, blank=True, null=True)
+
+    """def get_default_classroom():
+        return Classroom.objects.get(title="General").id
+
+    group = models.ForeignKey(Classroom, on_delete=models.CASCADE, default=get_default_classroom)"""
+    deadline = models.DateTimeField()
+    #assignment_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Question(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name="questions")
+    text = models.TextField()
+    is_mcq = models.BooleanField(default=False)
+    question_attachment = models.FileField(upload_to='question_attachments/', null=True, blank=True)
+
+    def __str__(self):
+        return self.text
+    
+class MCQOptions(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="options")
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+class Submission(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name="submissions")
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    mcq_answers = models.JSONField(null=True, blank=True)
+    descriptive_answers = models.JSONField(null=True, blank=True)
+    uploaded_files = models.FileField(upload_to='uploads/',blank=True,null=True)
+
+class Remark(models.Model):
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
+    text = models.TextField(blank=True,null=True)
+    given_at=models.DateTimeField(auto_now=True)
+
+class Notification(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications")
+    message = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Notification for {self.student.username} at {self.created_at}"
---
+++ assignments_app/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from .views import instructor_dashboard_asg, student_dashboard_asg, dashboard_redirect_asg, create_assignment, add_questions,edit_question,delete_question, finalize_assignment, delete_assignment, extend_deadline, submit_assignment, view_student_submission, view_submissions, add_remark, mark_notification_read, details_asg, submission_details
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "assignments_app"
+    
+urlpatterns = [
+    path('', dashboard_redirect_asg, name="dashboard_redirect_asg"),
+    path('instructor_asg/', instructor_dashboard_asg, name="instructor_dashboard_asg"),
+    path('student_asg/', student_dashboard_asg, name="student_dashboard_asg"),
+    path('create_assignment/', create_assignment, name="create_assignment"),
+    path('add_questions/<int:assignment_id>/', add_questions, name="add_questions"),
+    path('edit_question/<int:question_id>/', edit_question, name="edit_question"),
+    path('delete_question/<int:question_id>/', delete_question, name="delete_question"),
+    path('finalize_assignment/<int:assignment_id>/', finalize_assignment, name="finalize_assignment"),
+    path('details_asg/<int:assignment_id>/', details_asg, name="details_asg"),
+    path('delete_assignment/<int:assignment_id>/', delete_assignment, name="delete_assignment"),
+    path('extend_deadline/<int:assignment_id>/', extend_deadline, name="extend_deadline"),
+    path('submit_assignment/<int:assignment_id>/', submit_assignment, name="submit_assignment"),
+    path('view_submissions/<int:assignment_id>/', view_submissions, name="view_submissions"),
+    path('view_submission/<int:submission_id>/', view_student_submission, name="view_student_submission"),
+    path('add_remark/<int:submission_id>/', add_remark, name="add_remark"),
+    path('mark_notification_read/<int:notification_id>/', mark_notification_read, name="mark_notification_read"),
+    path('submission_details/<int:assignment_id>/',submission_details, name="submission_details"),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
